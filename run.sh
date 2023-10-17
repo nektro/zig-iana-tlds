a0f2392aac9f09347f050da5275ee153a2fc5bce
Meghan Denny <hello@nektro.net>
2023-10-16 20:26:35 -0700

    add run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu
+
+while true
+do
+    ./generate.sh
+    the_diff=$(git diff src/lib.zig | grep '^[-|+]' | grep -v 'src/lib.zig')
+    len=$(echo "$the_diff" | wc -l)
+
+    if (( $len > 2 ))
+    then
+        git add src/lib.zig
+        git commit -m "Automatic update"
+        git fetch
+        git pull --rebase
+        git push origin
+    fi
+
+    sleep 30m
+done
